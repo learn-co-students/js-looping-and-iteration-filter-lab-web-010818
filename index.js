12ef111a6b718140e934b4a44805f6808a579b38
Fei <fei.hafferkamp@gmail.com>
2018-02-19 15:42:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching (drivers, str) {
+  return drivers.filter (function (driver) {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+
+
+function fuzzyMatch (drivers, str) {
+  return drivers.filter (function (driver) {
+    return driver.startsWith(str);
+  });
+}
+
+function matchName (drivers, str) {
+  return drivers.filter (function (driver) {
+    return driver.name.toLowerCase() === str.toLowerCase();
+  });
+}
