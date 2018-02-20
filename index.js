76483c3298f25c4d0ec90648c5381a7cb2774f3a
Alexander Gutterman <alexanderf.gutterman@gmail.com>
2018-02-20 01:26:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(list, name) {
+  return list.filter(driverName => driverName.toLowerCase() === name.toLowerCase());
+}
+
+
+function fuzzyMatch(list, letters) {
+  return list.filter(name => name.slice(0, letters.length) === letters)
+}
+
+function matchName(list, name) {
+  debugger
+  return list.filter(driver => driver.name.toLowerCase() === name.toLowerCase() )
+}
