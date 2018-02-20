cc1bb21c19b513fdb303c41caa2d1689a984941a
katycarr <khcarr9@gmail.com>
2018-02-19 21:08:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file
+
+function findMatching(array, string) {
+  const matched = array.filter(function (driver) {return driver.toLowerCase() === string.toLowerCase()});
+  return matched;
+}
+
+function fuzzyMatch(array, string) {
+  const beginning = string.length
+  let matched;
+  debugger;
+  if (beginning !== 1) {
+    matched = array.filter(function (driver) {return driver.toLowerCase().slice(0, beginning) === string.toLowerCase()});
+  } else {
+    matched = array.filter(function (driver) {return driver.toLowerCase()[0] === string.toLowerCase()});
+  }
+  return matched;
+}
+
+function matchName(array,string) {
+  const matched = array.filter(function (driver) {return driver.name.toLowerCase() === string.toLowerCase()});
+  return matched;
+}
